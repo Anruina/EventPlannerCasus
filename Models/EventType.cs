7d181a53af4108b469190a9bc0a96368f353c2d5
MultyCoder2410 <tristanfranssen@gmail.com>
2024-01-18 17:25:39 +0100

    Changed models according to class diagram changes. Also added database migrations so the database can be generated

---
+++ Models/EventType.cs
@@ -0,0 +1,13 @@
+﻿namespace Library.Models
+{
+
+    public class EventType : TableData
+    {
+
+        public string? Name {  get; set; }
+
+        public List<Event>? Events {  get; set; }
+
+    }
+
+}
