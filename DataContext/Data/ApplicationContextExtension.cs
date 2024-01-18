a59cc6753c6375e006b023c28aee2dff135ed187
MultyCoder2410 <tristanfranssen@gmail.com>
2024-01-18 13:28:50 +0100

    Added ApplicationDbContext and put libraries in folder.

---
+++ DataContext/Data/ApplicationContextExtension.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace DataContext.Data
+{
+    public static class ApplicationContextExtension
+    {
+
+        /// <summary>
+        /// Makes it easy and quick to add the database to the project
+        /// </summary>
+        /// <param name="services">Service you are using</param>
+        /// <param name="connectionString">The connection string for the data databse</param>
+        /// <returns>The services given</returns>
+        public static IServiceCollection AddProgramContext(this IServiceCollection services, string connectionString)
+        {
+
+            services.AddDbContext<ApplicationDbContext>(options =>
+            {
+
+                options.UseSqlServer(connectionString);
+                options.LogTo(Console.WriteLine, new[] { Microsoft.EntityFrameworkCore.Diagnostics.RelationalEventId.CommandExecuting });
+
+            });
+
+            return services;
+
+        }
+
+    }
+}
---
+++ DataContext/Data/ApplicationDbContext.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace DataContext.Data
+{
+    
+    public class ApplicationDbContext : DbContext
+    {
+        
+        public ApplicationDbContext() { }
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+
+            optionsBuilder.UseSqlServer("Server=.;Database=EventPlannerCasusDatabase;Trusted_Connection=True;TrustServerCertificate=True");
+
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+
+            base.OnModelCreating(modelBuilder);
+
+        }
+
+    }
+
+}
