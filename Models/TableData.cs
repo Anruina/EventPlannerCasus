5023415c63e8b6a2ff893437e0ff2a3e92b2b2b5
MultyCoder2410 <tristanfranssen@gmail.com>
2024-01-18 17:25:39 +0100

    Changed models according to class diagram changes. Also added database migrations so the database can be generated

--- Models/TableData.cs
+++ Models/TableData.cs
@@ -1,15 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Models
+﻿namespace Library.Models
 {
     public class TableData
     {
 
-        //ID for every other object.
         public int Id { get; set; }
+
     }
 }
