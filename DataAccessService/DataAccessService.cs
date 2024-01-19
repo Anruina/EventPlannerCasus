b7d7679261a8906062f84f563225a44d34c8fd6d
Anruina <anruiamv@hotmail.com>
2024-01-19 13:28:09 +0100

    CRUD Organizer implemented

--- DataAccessService/DataAccessService.cs
+++ DataAccessService/DataAccessService.cs
@@ -2,6 +2,7 @@
 using Library.DataContext;
 using Microsoft.EntityFrameworkCore;
 using Library.DataContext.Data;
+using System.Runtime.CompilerServices;
 
 namespace Library.DataAccessService
 {
@@ -102,6 +103,74 @@ namespace Library.DataAccessService
 
         #endregion
 
+        #endregion
+
+        #region Organizer
+
+        #region Get
+        public async Task<Organizer?> GetOrganizer(string authenticationId)
+        {
+
+            if(_applicationContext?.Organizers == null)
+                return null;
+
+            Organizer? organizer = await _applicationContext.Organizers.Include(o => o.OrganizedEvents).FirstOrDefaultAsync(o => o.AuthenticationId == authenticationId);
+            
+            return organizer;
+
+        }
+        #endregion
+
+        #region Save
+        public async Task<Organizer?> SaveOrganizer(Organizer organizer)
+        {
+
+            if (_applicationContext?.Organizers == null)
+                return null;
+
+            _applicationContext.Organizers.Update(organizer);
+            await _applicationContext.SaveChangesAsync();
+
+            return organizer;
+
+        }
+
+        #endregion
+
+        #region Delete
+        public async Task<bool> DeleteOrganizer(string authenticationId)
+        {
+
+            if (_applicationContext?.Organizers == null)
+                return false;
+
+            Organizer? organizer = await _applicationContext.Organizers.FirstOrDefaultAsync(p => p.AuthenticationId == authenticationId);
+
+            if (organizer == null)
+                return false;
+
+            _applicationContext.Organizers.Remove(organizer);
+            await _applicationContext.SaveChangesAsync();
+
+            return true;
+
+        }
+        #endregion
+        #endregion
+
+        #region Activity
+        #endregion
+
+        #region Event
+        #endregion
+
+        #region EventType
+        #endregion
+
+        #region Address
+        #endregion
+
+        #region PlannedActivity
         #endregion
 
         #endregion
