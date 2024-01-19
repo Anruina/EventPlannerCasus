43e1a3f991bc6578f408ba93e565fa6cc79cfc1c
MultyCoder2410 <tristanfranssen@gmail.com>
2024-01-19 16:45:24 +0100

    Added usercontroller and ApiModels class library.

---
+++ Library.ApiModels/AccountModel.cs
@@ -0,0 +1,12 @@
+﻿namespace Library.ApiModels
+{
+
+    public class AccountModel
+    {
+
+        public string? Username { get; set; }
+        public string? Password { get; set; }
+
+    }
+
+}
