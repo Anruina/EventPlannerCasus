045a89dcdde649c76d9ded7ca7a41b67a49c7758
MultyCoder2410 <tristanfranssen@gmail.com>
2024-01-19 16:08:06 +0100

    Added GET for particapant.

--- EventPlannerAPI/Controllers/ParticipantsController.cs
+++ EventPlannerAPI/Controllers/ParticipantsController.cs
@@ -1,4 +1,6 @@
 ﻿using Library.DataAccessService;
+using Library.Models;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 
@@ -7,13 +9,13 @@ namespace EventPlannerAPI.Controllers
 
     [Route("api/[controller]")]
     [ApiController]
+    [Authorize]
     public class ParticipantsController : ControllerBase
     {
 
         #region Properties
 
         private readonly UserManager<IdentityUser>? _userManager;
-        private readonly SignInManager<IdentityUser>? _signInManager;
         private readonly DataAccessService _dataAccessService;
 
         #endregion
@@ -24,22 +26,47 @@ namespace EventPlannerAPI.Controllers
         /// Sets dataAccessService, user manager and signin manager
         /// </summary>
         /// <param name="userManager">Used for creating and deleting users</param>
-        /// <param name="signInManager">Used for loggin in and out</param>
         /// <param name="dataAccessService">Current DataAccessService used by program</param>
-        public ParticipantsController(UserManager<IdentityUser> userManager, SignInManager<IdentityUser> signInManager, DataAccessService dataAccessService)
+        public ParticipantsController(UserManager<IdentityUser> userManager, DataAccessService dataAccessService)
         {
 
             _userManager = userManager;
-            _signInManager = signInManager;
             _dataAccessService = dataAccessService;
 
         }
 
         #endregion
 
-        #region ACCOUNT
+        #region GET
 
-        
+        [HttpGet]
+        public async Task<ActionResult<Participant>> GetParticapant()
+        {
+
+            if (_userManager == null)
+                return StatusCode(StatusCodes.Status500InternalServerError, "Internal server error: UserManager is null.");
+
+            IdentityUser? user = await _userManager.GetUserAsync(User);
+
+            if (user == null)
+                return NotFound("No user was found.");
+
+            Participant? participant = await _dataAccessService.GetParticipant(user.Id);
+
+            if (participant == null)
+                return NotFound();
+
+            return participant;
+
+        }
+
+        #endregion
+
+        #region UPDATE
+
+        #endregion
+
+        #region DELETE
 
         #endregion
 
