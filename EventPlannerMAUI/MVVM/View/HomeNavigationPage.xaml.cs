923166514bb05d1d9c36fd7fba98667e4ea4c7e9
MultyCoder2410 <tristanfranssen@gmail.com>
2024-01-24 13:24:17 +0100

    Added pathfinding algorithm and floor 3 nodes. Also made it visual.

--- EventPlannerMAUI/MVVM/View/HomeNavigationPage.xaml.cs
+++ EventPlannerMAUI/MVVM/View/HomeNavigationPage.xaml.cs
@@ -2,8 +2,12 @@ namespace EventPlannerMAUI.MVVM.View;
 
 public partial class HomeNavigationPage : ContentPage
 {
+
 	public HomeNavigationPage()
 	{
-		InitializeComponent();
-	}
+
+		InitializeComponent(); 
+
+    }
+
 }
