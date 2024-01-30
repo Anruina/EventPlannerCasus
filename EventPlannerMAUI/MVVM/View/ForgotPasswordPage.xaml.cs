999ca9239baa683a6d7b8ea007e323f803e692e9
MultyCoder2410 <tristanfranssen@gmail.com>
2024-01-30 15:12:30 +0100

    Made some fixes in the code.

--- EventPlannerMAUI/MVVM/View/ForgotPasswordPage.xaml.cs
+++ EventPlannerMAUI/MVVM/View/ForgotPasswordPage.xaml.cs
@@ -3,25 +3,33 @@ namespace EventPlannerMAUI.MVVM.View;
 
     public partial class ForgotPasswordPage : ContentPage
 {
-    public ForgotPasswordPage() => InitializeComponent();
+    public ForgotPasswordPage()
+    {
+
+        InitializeComponent();
+
+    }
 
     private async void OnCancelClicked(object sender, EventArgs e)
     {
+
         await Navigation.PopAsync();
+
     }
 
     private async void OnResetClicked(object sender, EventArgs e)
     {
-        // Implementeer hier de logica om een reset link te sturen.
-        await DisplayAlert("Success", "Reset link sent to your email.", "OK");
 
-        // Optioneel: Navigeer terug naar login na succes.
-        await Navigation.PopAsync();
+        await DisplayAlert("Success", "Reset link sent to your email.", "OK");
+        await Navigation.PushAsync(new PasswordResetInstructionsPage());
+
     }
-    
+
     private async void OnInstructionsClicked(object sender, EventArgs e)
     { 
+
         await Navigation.PushAsync(new PasswordResetInstructionsPage()); 
+
     }
 }
 
--- EventPlannerMAUI/MVVM/View/PasswordResetInstructionsPage.xaml.cs
+++ EventPlannerMAUI/MVVM/View/PasswordResetInstructionsPage.xaml.cs
@@ -1,23 +1,24 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace EventPlannerMAUI.MVVM.View
+﻿namespace EventPlannerMAUI.MVVM.View
 {
+    
     public partial class PasswordResetInstructionsPage : ContentPage
     {
+
         public PasswordResetInstructionsPage()
         {
+
             InitializeComponent();
+
         }
 
         private async void OnBackToLoginClicked(object sender, EventArgs e)
         {
-            // Navigeer terug naar de loginpagina.
-            await Navigation.PopToRootAsync();
+
+            await Navigation.PopAsync();
+
         }
+
     }
+
 }
 
