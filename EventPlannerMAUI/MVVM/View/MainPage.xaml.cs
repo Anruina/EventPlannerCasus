05a7f899d8b302c9ff7d946e1a1667ab7ae3f763
Robin Buijsen <2105346buijsen@zuyd.nl>
2024-01-30 14:41:49 +0100

    Forgot Password Instructions

--- EventPlannerMAUI/MVVM/View/MainPage.xaml.cs
+++ EventPlannerMAUI/MVVM/View/MainPage.xaml.cs
@@ -17,6 +17,31 @@ namespace EventPlannerMAUI.MVVM.View
             InitializeComponent();
             _apiService = ServiceLocator.apiService;
 
+            OnCreate();
+
+        }
+
+        private void OnCreate()
+        {
+            OnCreate(loadingStackLayout: LoadingStackLayout);
+        }
+
+        private async void OnCreate(StackLayout loadingStackLayout)
+        {
+
+            if (await SecureStorage.GetAsync("Username") is string Username && await SecureStorage.GetAsync("Password") is string Password)
+            {
+
+                AccountModel? account = await _apiService.CreateObject("Api/User/Login", new AccountModel { Username = Username, Password = Password });
+ 
+                if (account != null)
+                    await Navigation.PushAsync(new HomeNavigationPage());
+
+            }
+
+            LoadingStackLayout.IsVisible = false;
+            LoginVerticalStackLayout.IsVisible = true;
+
         }
 
         private async void OnLoginClick(object sender, EventArgs e)
