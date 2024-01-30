7548c5d429224b0aef36bda3ff8c2b72f4bc7741
MultyCoder2410 <tristanfranssen@gmail.com>
2024-01-30 18:28:32 +0100

    Made pinching and panning work for map.

--- EventPlannerMAUI/MVVM/View/EventDetailMapPage.xaml.cs
+++ EventPlannerMAUI/MVVM/View/EventDetailMapPage.xaml.cs
@@ -3,11 +3,6 @@ namespace EventPlannerMAUI.MVVM.View;
 public partial class EventDetailMapPage : ContentPage
 {
 
-    double currentScale = 1;
-    double startScale = 1;
-    double xOffset = 0;
-    double yOffset = 0;
-
     public EventDetailMapPage()
     {
 
@@ -50,43 +45,37 @@ public partial class EventDetailMapPage : ContentPage
         if (e.Status == GestureStatus.Started)
         {
             
-            startScale = Content.Scale;
-            Content.AnchorX = 0;
-            Content.AnchorY = 0;
+            MapView.AnchorX = 0;
+            MapView.AnchorY = 0;
 
         }
-        if (e.Status == GestureStatus.Running)
+        else if (e.Status == GestureStatus.Running)
         {
 
-            currentScale += (e.Scale - 1) * startScale;
-            currentScale = Math.Max(1, currentScale);
+            MapView.Scale *= e.Scale;
 
-            double renderedX = Content.X + xOffset;
-            double deltaX = renderedX / Width;
-            double deltaWidth = Width / (Content.Width * startScale);
-            double originX = (e.ScaleOrigin.X - deltaX) * deltaWidth;
+            if (MapView.Scale > 1.6f || MapView.Scale < 0.85f)
+                MapView.Scale /= e.Scale;
+            else
+            {
 
-            double renderedY = Content.Y + yOffset;
-            double deltaY = renderedY / Height;
-            double deltaHeight = Height / (Content.Height * startScale);
-            double originY = (e.ScaleOrigin.Y - deltaY) * deltaHeight;
+                MapView.WidthRequest *= e.Scale;
+                MapView.HeightRequest *= e.Scale;
 
-            double targetX = xOffset - (originX * Content.Width) * (currentScale - startScale);
-            double targetY = yOffset - (originY * Content.Height) * (currentScale - startScale);
-
-            Content.TranslationX = Math.Clamp(targetX, -Content.Width * (currentScale - 1), 0);
-            Content.TranslationY = Math.Clamp(targetY, -Content.Height * (currentScale - 1), 0);
-
-            Content.Scale = currentScale;
+            }
 
         }
-        if (e.Status == GestureStatus.Completed)
-        {
 
-            xOffset = Content.TranslationX;
-            yOffset = Content.TranslationY;
+    }
 
-        }
+    private void OnMapPan(object sender, PanUpdatedEventArgs e)
+    {
+
+        if ((e.TotalX < 0.0f && MapView.TranslationX > -MapView.WidthRequest + 500) || (e.TotalX > 0.0f && MapView.TranslationX < 0))
+            MapView.TranslationX += e.TotalX;
+
+        if ((e.TotalY < 0.0f && MapView.TranslationY > -MapView.HeightRequest + 600) || (e.TotalY > 0.0f && MapView.TranslationY < MapView.HeightRequest - 500))
+            MapView.TranslationY += e.TotalY;
 
     }
 
