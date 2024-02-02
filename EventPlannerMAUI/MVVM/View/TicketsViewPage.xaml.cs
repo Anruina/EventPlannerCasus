127904cd526a75b1d689e965277aaef966bb2196
Anruina <anruiamv@hotmail.com>
2024-02-02 10:40:02 +0100

    Started with cardview for tickets

--- EventPlannerMAUI/MVVM/View/TicketsViewPage.xaml.cs
+++ EventPlannerMAUI/MVVM/View/TicketsViewPage.xaml.cs
@@ -1,10 +1,32 @@
+using EventPlannerMAUI.MobileApp;
+using Library.ApiService;
+using Library.Models;
+
 namespace EventPlannerMAUI.MVVM.View;
 
 public partial class TicketsViewPage : ContentPage
 {
+
+	private readonly ApiService _apiService;
+
 	public TicketsViewPage()
 	{
+
 		InitializeComponent();
+		_apiService = ServiceLocator.apiService;
+
+		OnCreate();
+
 	}
+
+	private async void OnCreate()
+	{
+
+        Participant? participant = await _apiService.GetSpecific<Participant>("Api/Participants");
+
+		if (participant != null)
+			EventTicketListView.BindingContext = participant.VisitedEvents;
+
+    }
 
 }
