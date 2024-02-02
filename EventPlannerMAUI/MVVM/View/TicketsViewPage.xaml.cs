02fac46268e241352a6433cd57529353b1a9c92c
Anruina <anruiamv@hotmail.com>
2024-02-02 11:07:54 +0100

    Continued cardview for ticketpage

--- EventPlannerMAUI/MVVM/View/TicketsViewPage.xaml.cs
+++ EventPlannerMAUI/MVVM/View/TicketsViewPage.xaml.cs
@@ -1,10 +1,32 @@
+using EventPlannerMAUI.MobileApp;
+using Library.ApiService;
+using Library.Models;
+
 namespace EventPlannerMAUI.MVVM.View;
 
 public partial class TicketsViewPage : ContentPage
 {
+
+	private readonly ApiService _apiService;
+
 	public TicketsViewPage()
 	{
+
 		InitializeComponent();
+		_apiService = ServiceLocator.apiService;
+
+		OnCreate();
+
 	}
+
+	private async void OnCreate()
+	{
+
+        User? user = await _apiService.GetSpecific<User>("Api/User");
+
+		if (user != null)
+			EventTicketListView.BindingContext = user.VisitedEvents;
+
+    }
 
 }
