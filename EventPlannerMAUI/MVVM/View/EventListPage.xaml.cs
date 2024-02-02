a78631b2c9b4ab11f550b5117bb21deb1931fb02
Alex045jeweetzelf <113170875+Alex045jeweetzelf@users.noreply.github.com>
2024-02-02 11:33:08 +0100

    Added search bar to event

--- EventPlannerMAUI/MVVM/View/EventListPage.xaml.cs
+++ EventPlannerMAUI/MVVM/View/EventListPage.xaml.cs
@@ -1,62 +1,60 @@
+using System;
 using System.Collections.ObjectModel;
 using EventPlannerMAUI.MobileApp;
 using Library.ApiService;
 using Library.Models;
+using Microsoft.Maui.Controls;
 
-namespace EventPlannerMAUI.MVVM.View;
-
-public partial class EventListPage : ContentPage
+namespace EventPlannerMAUI.MVVM.View
 {
-
-    private readonly ApiService _apiService;
-
-    public EventListPage()
+    public partial class EventListPage : ContentPage
     {
+        private readonly ApiService _apiService;
+        private ObservableCollection<Event> _allEvents;
 
-        InitializeComponent();
-        _apiService = ServiceLocator.apiService;
-
-        OnCreate();
-
-    }
-
-    private async void OnCreate()
-    {
-
-        EventListView.ItemsSource = await _apiService.GetList<Event>("Api/Events");
-        
-        Organizer? organizer = await _apiService.GetSpecific<Organizer>("Api/Organizers");
-        if (organizer != null)
-            AddEventButton.IsVisible = true;
-
-    }
-
-    protected override async void OnAppearing()
-    {
-        
-        base.OnAppearing();
-        EventListView.ItemsSource = await _apiService.GetList<Event>("Api/Events");
-
-    }
-
-    private async void OnAddEventClicked(object sender, EventArgs e)
-    {
-
-        await Navigation.PushAsync(new SaveEventPage());
-
-    }
-
-    private async void OnSelectedEvent(object sender, EventArgs e)
-    {
-
-        if (EventListView.SelectedItem != null)
+        public EventListPage()
         {
-
-            await Navigation.PushAsync(new NavigationPage(new EventDetailTabbedPage()));
-            EventListView.SelectedItem = null;
-
+            InitializeComponent();
+            _apiService = ServiceLocator.apiService;
+            OnCreate();
         }
 
-    }
+        private async void OnCreate()
+        {
+            _allEvents = new ObservableCollection<Event>(await _apiService.GetList<Event>("Api/Events"));
+            EventListView.ItemsSource = _allEvents;
 
+            Organizer? organizer = await _apiService.GetSpecific<Organizer>("Api/Organizers");
+            if (organizer != null)
+                AddEventButton.IsVisible = true;
+        }
+
+        protected override async void OnAppearing()
+        {
+            base.OnAppearing();
+            _allEvents = new ObservableCollection<Event>(await _apiService.GetList<Event>("Api/Events"));
+            EventListView.ItemsSource = _allEvents;
+        }
+
+        private async void OnAddEventClicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new SaveEventPage());
+        }
+
+        private async void OnSelectedEvent(object sender, EventArgs e)
+        {
+            if (EventListView.SelectedItem != null)
+            {
+                await Navigation.PushAsync(new NavigationPage(new EventDetailTabbedPage()));
+                EventListView.SelectedItem = null;
+            }
+        }
+
+        private void OnSearchButtonPressed(object sender, EventArgs e)
+        {
+            string searchTerm = searchBar.Text.ToLower();
+            var filteredEvents = _allEvents.Where(eventItem => eventItem.Name.ToLower().Contains(searchTerm)).ToList();
+            EventListView.ItemsSource = new ObservableCollection<Event>(filteredEvents);
+        }
+    }
 }
